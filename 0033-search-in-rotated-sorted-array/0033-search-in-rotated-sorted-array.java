fc10f4175b36cb966a8a638c26915b3e52fdd9af
Sakshi Parihar <pariharsakshi36017@gmail.com>
2026-08-27 23:39:15 +0530

    Time: 0 ms (100%), Space: 43.9 MB (28.16%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,33 +1,41 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int minimum=Integer.MAX_VALUE;
-        int min_index=0;
-        for(int i=0;i<nums.length;i++){
-           if(minimum>nums[i]){
-            minimum=nums[i];
-            min_index=i;
-           }
+        if(nums.length==0){
+            return -1;
         }
-        int l = 0;
-        int r = 0;
-        if (target >= nums[min_index] && target <= nums[nums.length - 1]) {
-            l = min_index;
-            r = nums.length - 1;
-        } else {
-            l = 0;
-            r = min_index;
+       int l=0;
+       int r=nums.length-1;
+       //search min index
+       while(l<r){
+        int mid=l+(r-l)/2;
+        if(nums[mid]>nums[r]){
+            l=mid+1;
+        }else{
+            r=mid;
         }
+       }
+       int min=l;
 
-        while (l <= r) {
-            int mid =l+(r-l)/2;
-            if (nums[mid] == target) {
-                return mid;
-            } else if (target > nums[mid]) {
-                l = mid + 1;
-            } else {
-                r = mid - 1;
-            }
+       //choose the side of the array
+       if(target>=nums[min] && target<=nums[nums.length-1]){
+        l=min;
+        r=nums.length-1;
+       } else{
+        l=0;
+        r=min-1;
+       }
+
+       while(l<=r){
+        int mid=l+(r-l)/2;
+        if(nums[mid]==target){
+            return mid;
         }
-        return -1;
+        else if(nums[mid]<target){
+            l=mid+1;
+        }else{
+            r=mid-1;
+        }
+       }
+       return -1;
     }
 }
