8cf77676987aec68cac20e07b8c1d9e2cc41e57f
Sakshi Parihar <pariharsakshi36017@gmail.com>
2026-09-11 23:49:59 +0530

    Time: 4 ms (86.93%), Space: 43.3 MB (99.87%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -1,32 +1,32 @@
 class Solution {
-    public int ans=0;
-    public void helper(int[] digits,int len,boolean vis[],int number){
-        if(len==3){
-            if(number%2==0){
+    public int ans = 0;
+
+    public void helper(int[] digits, int len, boolean vis[], int number) {
+        if (len == 3) {
+            if (number % 2 == 0) {
                 ans++;
             }
             return;
         }
-        for(int i=0;i<digits.length;i++){
-            if(i>0 && digits[i]==digits[i-1] && !vis[i-1]){
+        for (int i = 0; i < digits.length; i++) {
+            if (i > 0 && digits[i] == digits[i - 1] && !vis[i - 1]) {
                 continue;
             }
-            if(vis[i]){
+            if (vis[i]) {
                 continue;
             }
-            if(len==0 && digits[i]==0){
+            if (len == 0 && digits[i] == 0) {
                 continue;
             }
-            if(!vis[i]){
-                vis[i]=true;
-                helper(digits,len+1,vis,number*10+digits[i]);
-                vis[i]=false;  
-            }
+            vis[i] = true;
+            helper(digits, len + 1, vis, number * 10 + digits[i]);
+            vis[i] = false;
         }
     }
+
     public int totalNumbers(int[] digits) {
         Arrays.sort(digits);
-        helper(digits,0,new boolean[digits.length],0);
+        helper(digits, 0, new boolean[digits.length], 0);
         return ans;
     }
 }
